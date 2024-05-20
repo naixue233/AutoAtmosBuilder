f2959b8d29c622bcd51a6bd7ce4a6ac1f61353a6
naixue233 <122972585+naixue233@users.noreply.github.com>
2024-05-20 20:21:17 +0800

    Update AutoAtmosBuilder.sh

--- AutoAtmosBuilder.sh
+++ AutoAtmosBuilder.sh
@@ -58,17 +58,6 @@ else
     unzip -oq atmosphere.zip
     rm atmosphere.zip
 fi
-curl -sL https://api.github.com/repos/Atmosphere-NX/Atmosphere/releases \
-  | jq '.[1] | .name' \
-curl -sL https://api.github.com/repos/Atmosphere-NX/Atmosphere/releases \
-  | jq '.[1].assets' | jq '.[1].browser_download_url' \
-  | xargs -I {} curl -sL {} -o fusee.bin
-if [ $? -ne 0 ]; then
-    echo "fusee.bin download\033[31m failed\033[0m."
-else
-    echo "fusee.bin download\033[32m success\033[0m."
-    mv fusee.bin ./bootloader/payloads
-fi
 ### Fetch latest Hekate + Nyx Chinese from https://github.com/easyworld/hekate/releases/latest
 curl -sL https://api.github.com/repos/easyworld/hekate/releases/latest \
   | jq '.name' \
