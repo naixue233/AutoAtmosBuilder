158a21aa6874bb26122dc62a24ec48c28016fceb
naixue233 <122972585+naixue233@users.noreply.github.com>
2024-05-20 18:04:21 +0800

    Update AutoAtmosBuilder.sh

--- AutoAtmosBuilder.sh
+++ AutoAtmosBuilder.sh
@@ -101,7 +101,7 @@ else
     echo "boot.dat-Resources download\033[32m success\033[0m."
 fi
 ### Fetch latest SigPatches.zip from https://raw.github.com/naixue233/naixue_nx_atm_Auto_Script/main/resources/
-curl -sL https://raw.github.com/naixue233/naixue_nx_atm_Auto_Script/main/resources/sigpatches.zip -o sigpatches.zip
+curl -sL https://sigmapatches.su/sigpatches.zip -o sigpatches.zip
 if [ $? -ne 0 ]; then
     echo "SigPatches download\033[31m failed\033[0m."
 else
