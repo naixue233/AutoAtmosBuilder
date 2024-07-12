4bb6630257c1f39b7a0668d2e81e006df3c27698
naixue233 <122972585+naixue233@users.noreply.github.com>
2024-07-12 21:40:51 +0800

    Update AutoAtmosBuilder.sh

--- AutoAtmosBuilder.sh
+++ AutoAtmosBuilder.sh
@@ -89,6 +89,36 @@ else
     rm MissionControl.zip
 fi
 
+### Fetch latest ldn_mitm from https://api.github.com/repos/spacemeowx2/ldn_mitm/releases/latest
+curl -sL https://api.github.com/repos/spacemeowx2/ldn_mitm/releases/latest \
+  | jq '.tag_name' \
+  | xargs -I {} echo ldn_mitm {} >> ../description.txt
+curl -sL https://api.github.com/repos/spacemeowx2/ldn_mitm/releases/latest \
+  | jq '.assets' | jq '.[0].browser_download_url' \
+  | xargs -I {} curl -sL {} -o ldn_mitm.zip
+if [ $? -ne 0 ]; then
+    echo "ldn_mitm download\033[31m failed\033[0m."
+else
+    echo "ldn_mitm download\033[32m success\033[0m."
+    unzip -oq ldn_mitm.zip
+    rm ldn_mitm.zip
+fi
+
+### Fetch latest ldn_mitm from https://api.github.com/repos/masagrator/SaltyNX/releases/latest
+curl -sL https://api.github.com/repos/masagrator/SaltyNX/releases/latest \
+  | jq '.name' \
+  | xargs -I {} echo {} >> ../description.txt
+curl -sL https://api.github.com/repos/masagrator/SaltyNX/releases/latest \
+  | jq '.assets' | jq '.[0].browser_download_url' \
+  | xargs -I {} curl -sL {} -o SaltyNX.zip
+if [ $? -ne 0 ]; then
+    echo "ldn_mitm download\033[31m failed\033[0m."
+else
+    echo "ldn_mitm download\033[32m success\033[0m."
+    unzip -oq SaltyNX.zip
+    rm SaltyNX.zip
+fi
+
 ### Fetch latest SysDVR from hhttps://github.com/exelix11/SysDVR
 curl -sL https://api.github.com/repos/exelix11/SysDVR/releases/latest \
   | jq '.tag_name' \
